436e7064662772745429dd11699396a3b8fd9986
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-05-31 16:04:18 +0300

    fix: оставшиеся ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 /* получаем доступ к DOM элементам profile и popup */
-let profile = document.querySelector(".profile");
-let popup = document.querySelector(".popup");
+const profile = document.querySelector(".profile");
+const popup = document.querySelector(".popup");
 
 /* получаем доступ к кнопкам edit, save и close */
-let editButton = profile.querySelector(".profile__edit-button");
-let saveButton = popup.querySelector(".popup__save-button");
-let closeButton = popup.querySelector(".popup__close-button");
+const editButton = profile.querySelector(".profile__edit-button");
+const closeButton = popup.querySelector(".popup__close-button");
 
 /* получаем доступ к name и bio пользователя */
-let profileName = profile.querySelector(".profile__name");
-let profileBio = profile.querySelector(".profile__bio");
+const profileName = profile.querySelector(".profile__name");
+const profileBio = profile.querySelector(".profile__bio");
 
 /* получаем доступ к полям ввода */
-let formItemName = popup.querySelector(".popup__form-item_value_name");
-let formItemBio = popup.querySelector(".popup__form-item_value_bio");
+const formItemName = popup.querySelector(".popup__form-item_value_name");
+const formItemBio = popup.querySelector(".popup__form-item_value_bio");
 
 /* создаем функцию для открытия popup'a */
-function popupUnHidden() {
-  popup.classList.remove("popup_opened");
+function openPopup() {
+  popup.classList.add("popup_opened");
   formItemName.value = profileName.textContent;
   formItemBio.value = profileBio.textContent;
 }
 
 /* создаем функцию для скрытия  popup'a */
-function popupHidden() {
-  popup.classList.add("popup_opened");
+function closePopup() {
+  popup.classList.remove("popup_opened");
 }
 
 /* функция отправки формы */
@@ -33,10 +32,10 @@ function formSubmitHandler(evt) {
   profileName.textContent = formItemName.value;
   profileBio.textContent = formItemBio.value;
 
-  popupHidden();
+  closePopup();
 }
 
 /* добавляем обработчики событий */
-editButton.addEventListener("click", popupUnHidden);
-closeButton.addEventListener("click", popupHidden);
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
 popup.addEventListener("submit", formSubmitHandler);
