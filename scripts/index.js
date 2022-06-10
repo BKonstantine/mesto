54a62e6a5c41c11349a6413470bf57c18c214385
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-06-10 18:51:19 +0300

    feat: найти в html блоки с карточками и шаблон карт

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+/* стартовый набор для блока card */
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
 /* получаем доступ к DOM элементам profile и popup */
 const profile = document.querySelector(".profile");
 const popup = document.querySelector(".popup");
@@ -14,6 +42,15 @@ const profileBio = profile.querySelector(".profile__bio");
 const formItemName = popup.querySelector(".popup__form-item_value_name");
 const formItemBio = popup.querySelector(".popup__form-item_value_bio");
 
+/* получаем доступ к секции photo-grid */
+const photoGrid = document.querySelector(".photo-grid");
+
+/* получаем доступ к шаблону карточек */
+const cardTemplate = document.querySelector(".card-template").content;
+
+/* клонируем содержимое шаблона */
+const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+
 function togglePopup() {
   if (popup.classList.contains("popup_opened") === false) {
     popup.classList.toggle("popup_opened");
@@ -24,25 +61,12 @@ function togglePopup() {
   }
 }
 
-/* создаем функцию для открытия popup'a */
-/* function openPopup() {
-  popup.classList.add("popup_opened");
-  formItemName.value = profileName.textContent;
-  formItemBio.value = profileBio.textContent;
-} */
-
-/* создаем функцию для скрытия  popup'a */
-/* function closePopup() {
-  popup.classList.remove("popup_opened");
-} */
-
 /* функция отправки формы */
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = formItemName.value;
   profileBio.textContent = formItemBio.value;
 
-  /* closePopup(); */
   togglePopup();
 }
 
